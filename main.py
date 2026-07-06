190f5f134509946eba339711fc6e3cbae79f24c7
Lestat <147524600+Lestatnick@users.noreply.github.com>
2026-07-06 20:01:19 -0300

    ajuste no nome para o import do realizar emprestimos

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from modules.gerenciadorLivros import (
+from models.modules.gerenciadorLivros import (
     cadastrar_livro,
     listar_livros,
     buscar_livro
 )
 
-from modules.usuarios import (
+from models.usuarios import (
     cadastrar_aluno,
     listar_alunos,
     buscar_aluno
 )
 
-from modules.realizar_emprestimos import realizar_emprestimo
+from models.modules.realizar_emprestimos import realizar_emprestimo
 from modules.devolucoes import devolver_livro
 from modules.historico import listar_historico
 from modules.relatorios import menu_relatorios
